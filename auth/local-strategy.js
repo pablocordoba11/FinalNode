efbc8453ac4a006380d3a207af31649619205de3
Pablo <pablocordoba11@gmail.com>
2015-05-20 20:37:28 -0300

    Creación de ramaB

--- auth/local-strategy.js
+++ auth/local-strategy.js
@@ -14,15 +14,16 @@ var passport = module.parent.exports.passport,
 		usernameField: 'email',
 		passwordField: 'pass1'
 	},
-	function(username, password , done){
-		console.log("entra function"+ username + password);
-		Empleado.findOne({email:username}, function(err, emp){
-			console.log(emp, err, 'SA')
+	function(email, pass1 , done){
+		console.log("entra function: "+ email + " Password: " + pass1);
+		//Problemas con el findOne
+		Empleado.findOne({email:email}, function(err, emp){
+			console.log(emp, err, 'SA');
 			if(err) {return done(err);}
 			if(!emp){
 				return done(null,false,{message :'E-mail no valido'});
 			}
-			if(!emp.authenticate(password)){
+			if(!emp.authenticate(pass1)){
 				return done(null,false, {message:'Password incorrecto'});
 			}
 			return done(null,emp);
