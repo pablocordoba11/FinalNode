1ae0f86098f3d1bdef86671dcfa46357e2b7f4e7
Pablo <pablocordoba11@gmail.com>
2015-05-21 00:19:38 -0300

    Barra de navegacion

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,10 @@
 var app = module.parent.exports.app;
 var Empleado = require('../models/employee.js');
 var passport = module.parent.exports.passport;
-
+// get index 
+app.get('/index',function (req, res) {
+	res.render('index',{title:'Home'});
+});
 //get Login
 app.get('/admin',function(req, res){
 //Empleado.find({}, function(err, docs){
